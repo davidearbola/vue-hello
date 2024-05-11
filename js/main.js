75ffb82e6cdda4240a5d542c8076ed881e7bf2a7
davidearbola <davidearbola@gmail.com>
2024-05-11 21:22:58 +0200

    extra - cambio colore del nome che viene inserito tramite input e cambio la foto sempre al click del bottone

--- js/main.js
+++ js/main.js
@@ -3,9 +3,26 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			titolo: "Benvenuto nel mondo di VueJS!",
-			sottotitolo: "Ora sei pronto a diventare pazzo veramente",
-			img: "https://picsum.photos/200/300",
+			titolo: "Benvenuto",
+			sottotitolo: "nel mondo di VueJS!",
+			img: "./img/logo.webp",
+			firstName: "",
+			lastName: "",
+			classe: "red",
 		};
+	},
+	methods: {
+		cambiaColore() {
+			if (this.classe == "red") {
+				this.classe = "green";
+			} else if (this.classe == "green") {
+				this.classe = "red";
+			}
+			if (this.img == "./img/logo.webp") {
+				this.img = "./img/logo2.jpeg";
+			} else if (this.img == "./img/logo2.jpeg") {
+				this.img = "./img/logo.webp";
+			}
+		},
 	},
 }).mount("#app");
