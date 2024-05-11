420d0b9cc170251e099b5eec58c4acfd2e5c32a5
davidearbola <davidearbola@gmail.com>
2024-05-11 20:25:36 +0200

    Bonus - aggiungo immagine usado i dati

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ createApp({
 		return {
 			titolo: "Benvenuto nel mondo di VueJS!",
 			sottotitolo: "Ora sei pronto a diventare pazzo veramente",
+			img: "https://picsum.photos/200/300",
 		};
 	},
 }).mount("#app");
